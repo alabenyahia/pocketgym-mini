3363b838eb480d0b134eec8dbba609c00aa7dc53
Ala ben yahia <alabinyahia@gmail.com>
2023-12-11 16:22:21 +0100

    Added theme combined with reactnavigation and reactnativepaper

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
 import 'react-native-gesture-handler'
-import {NavigationContainer} from "@react-navigation/native";
 import {AppDrawer} from "./routing/AppDrawer"
+import {
+    NavigationContainer,
+    DarkTheme as NavigationDarkTheme,
+    DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+    adaptNavigationTheme,
+    MD3DarkTheme,
+    MD3LightTheme,
+    PaperProvider
+} from 'react-native-paper';
+import merge from 'deepmerge';
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+    reactNavigationLight: NavigationDefaultTheme,
+    reactNavigationDark: NavigationDarkTheme,
+});
+
+//COMBINE reactnativepaper theme and reactnavigation theme
+const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
+const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
+
+const lightTheme = {
+    ...CombinedDefaultTheme,
+    colors: {
+        ...CombinedDefaultTheme.colors,
+        primary: '#627f9d',
+        secondary: '#9D8062'
+    },
+};
+
+const darkTheme = {
+    ...CombinedDarkTheme,
+    colors: {
+        ...CombinedDarkTheme.colors,
+        primary: '#627f9d',
+        secondary: '#9D8062'
+    },
+};
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <AppDrawer/>
-        </NavigationContainer>
+        <PaperProvider theme={darkTheme}>
+            <NavigationContainer theme={darkTheme}>
+                <AppDrawer/>
+            </NavigationContainer>
+        </PaperProvider>
     );
 }
 
--- routing/AppDrawer.js
+++ routing/AppDrawer.js
@@ -1,16 +1,21 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {Dashboard} from "../screens/Dashboard";
 import {Memberships} from "../screens/Memberships";
 import {Members} from "../screens/Members";
 
 const MyDrawer = createDrawerNavigator();
 
-export  function AppDrawer() {
+export function AppDrawer() {
     return (
-        <MyDrawer.Navigator initialRouteName="Dashboard">
-            <MyDrawer.Screen name="Dashboard" component={Dashboard} />
-            <MyDrawer.Screen name="Memberships" component={Memberships} />
-            <MyDrawer.Screen name="Members" component={Members} />
+        <MyDrawer.Navigator initialRouteName="Dashboard"
+                            screenOptions={{
+                                headerTintColor: '#e7e7e7',
+                                headerStyle: {backgroundColor: '#627f9d'},
+                                drawerStyle: {backgroundColor: '#334352'}
+                            }}>
+            <MyDrawer.Screen name="Dashboard" component={Dashboard}/>
+            <MyDrawer.Screen name="Memberships" component={Memberships}/>
+            <MyDrawer.Screen name="Members" component={Members}/>
         </MyDrawer.Navigator>
     );
 }
